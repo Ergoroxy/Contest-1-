9ca7a43d93c46effe339bc9ff30fb902425071f2
Ergoroxy <161676517+Ergoroxy@users.noreply.github.com>
2025-02-12 20:47:55 +0500

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,14 +1,14 @@
 def typeBasedTransformer(**kwargs):
     Output = {}
     for key, value in kwargs.items():
-        if isinstance(value, (int, float)):
+        if isinstance(value, (int, float)):   # Makes a square 
             Output[key] = value ** 2
         elif isinstance(value, str):
-            Output[key] = value[::-1]
+            Output[key] = value[::-1]  # Reverses string
         elif isinstance(value, bool):
-            Output[key] = not value
+            Output[key] = not value    # Invert the value
         elif isinstance(value, (list, tuple)):
-            Output[key] = value[::-1]
+            Output[key] = value[::-1]  # Reverses numbers 
         elif isinstance(value, dict):
             if len(set(value.values())) == len(value.values()):  # Ensure unique values
                 Output[key] = {v: k for k, v in value.items()}
